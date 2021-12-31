a0d34400930815a903ed1aafec52cc87af7f06c9
Arquimedes Ferreira <arquimedes.ferreira.sousa@gmail.com>
2021-12-31 18:46:43 -0300

    Add files to project js_start

--- js_start/contador/assets/js/script.js
+++ js_start/contador/assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("Apenas teste do Console !")
+//console.log("Inciando contador !")
+
+
+var number  = document.getElementById("number")
+var numberInt = 0
+
+//document.getElementById("btn-increment")
+
+function increment(){
+	//console.log("Adicionado um número")
+	numberInt += 1
+	number.innerHTML = numberInt
+	if(numberInt > 0){
+		number.style.backgroundColor="lightblue"
+	}
+	//console.log(number.textContent)
+}
+
+function decrement(){
+	//console.log("Decrementando o número ")
+	numberInt -= 1
+	number.innerHTML = numberInt
+	if(numberInt < 0){
+		number.style.backgroundColor = "red"
+	}
+	//console.log(number.textContent)
+}
